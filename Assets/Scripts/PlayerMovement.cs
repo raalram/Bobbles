03d011995c3966cb0b7877e9dfa84346520f5897
Rakel <raalram@gmail.com>
2025-03-04 00:33:51 +0100

    correcciones minimapa y movimiento jugador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(0,0,velocidad*Time.deltaTime);
+            transform.Translate(0,0,-velocidad*Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate( -velocidad * Time.deltaTime,0,0);
+            transform.Translate( velocidad * Time.deltaTime,0,0);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(0, 0,  -velocidad * Time.deltaTime);
+            transform.Translate(0, 0,  velocidad * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(velocidad * Time.deltaTime, 0, 0);
+            transform.Translate(-velocidad * Time.deltaTime, 0, 0);
         }
 
         transform.Rotate(0, Input.GetAxis("Mouse X") * sensibilidad * Time.deltaTime, 0);
